45b4846f88d59db8f6b9df0cdba55509054d44d8
leiserfg <leiserfg@gmail.com>
2022-11-28 09:09:45 +0100

    More nix settings

--- shared/nix.nix
+++ shared/nix.nix
@@ -1,5 +1,8 @@
-{...}: {
+{lib, ...}: {
   nix.settings = {
+
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
