64d2afdcce1490e01885ad719dfad635e7fc8c2c
leiserfg <leiserfg@gmail.com>
2022-11-30 17:34:54 +0100

    Wed 30 Nov 2022 05:34:54 PM CET

--- home/leiserfg/dunkel.nix
+++ home/leiserfg/dunkel.nix
@@ -6,7 +6,7 @@
     mapExpression = {Control_L = "Escape";};
   };
   home.keyboard.options = ["ctrl:nocaps"];
-  home.packages = with pkgs; [slack pre-commit poetry terraform];
+  home.packages = with pkgs; [slack pre-commit poetry terraform iredis];
   xresources.extraConfig = ''
     Xft.dpi:       128
     *dpi:          128
