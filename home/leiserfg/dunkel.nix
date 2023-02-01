039349364f8fc9640ccec5bb47fe90788caeffa3
leiserfg <leiserfg@gmail.com>
2023-02-01 12:06:55 +0100

    Insomnia

--- home/leiserfg/dunkel.nix
+++ home/leiserfg/dunkel.nix
@@ -6,7 +6,7 @@
     mapExpression = {Control_L = "Escape";};
   };
   home.keyboard.options = ["ctrl:nocaps"];
-  home.packages = with pkgs; [slack pgcli pre-commit poetry terraform iredis bluetuith terraform-ls];
+  home.packages = with pkgs; [slack pgcli pre-commit poetry terraform iredis bluetuith terraform-ls insomnia];
   xresources.extraConfig = ''
     Xft.dpi:       128
     *dpi:          128
