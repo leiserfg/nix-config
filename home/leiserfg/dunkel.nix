f01bbea4259c5fbc3c277cef2bbc4105a45c90e2
leiserfg <leiserfg@gmail.com>
2023-03-31 18:04:15 +0200

    Update

--- home/leiserfg/dunkel.nix
+++ home/leiserfg/dunkel.nix
@@ -6,7 +6,7 @@
     mapExpression = {Control_L = "Escape";};
   };
   home.keyboard.options = ["ctrl:nocaps"];
-  home.packages = with pkgs; [slack pgcli pre-commit poetry terraform terraform-ls insomnia awscli2 csvkit];
+  home.packages = with pkgs; [slack pgcli pre-commit poetry terraform terraform-ls insomnia awscli2 csvkit libreoffice];
   xresources.extraConfig = ''
     Xft.dpi:       128
     *dpi:          128
