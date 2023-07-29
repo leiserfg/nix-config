26ef3c9dc794328b4fcd59806d0a49f34896bd67
leiserfg <leiserfg@gmail.com>
2023-07-29 20:24:53 +0200

    Sat Jul 29 08:24:53 PM CEST 2023

--- hosts/common/global/x11.nix
+++ hosts/common/global/x11.nix
@@ -6,20 +6,20 @@
   outputs,
   ...
 }: {
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    xkbVariant = "altgr-intl";
-    displayManager = {
-      /*
-      lightdm.enable = true;
-      */
-    };
-    /*
-    windowManager.tinywm.enable = true;
-    */
-    libinput.enable = true;
-  };
+ # services.xserver = {
+ #   enable = true;
+ #   layout = "us";
+ #   xkbVariant = "altgr-intl";
+ #   displayManager = {
+ #     /*
+ #     lightdm.enable = true;
+ #     */
+ #   };
+ #   /*
+ #   windowManager.tinywm.enable = true;
+ #   */
+ #   libinput.enable = true;
+ # };
   programs.dconf.enable = true;
 
   xdg.portal = {
