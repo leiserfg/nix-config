609b78ca5139f689c7a2bbdd269ac6ddbf2a0cb6
leiserfg <leiserfg@gmail.com>
2023-12-01 09:30:50 +0100

    23.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "My nix config";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     hardware.url = "github:nixos/nixos-hardware";
 
