f4c55dde76d016c340d04796d3f061cba9198f01
leiserfg <leiserfg@gmail.com>
2024-02-05 09:53:00 +0100

    Mon Feb  5 09:53:00 AM CET 2024

--- home/leiserfg/common.nix
+++ home/leiserfg/common.nix
@@ -46,7 +46,7 @@
     nodePackages.typescript-language-server
 
     iw
-
+    dmidecode
     vokoscreen-ng
     zoxide
     iredis
@@ -251,11 +251,17 @@
         yolo = {
           settings = {
             "browser.compactmode.show" = true;
-            "media.ffmpeg.vaapi.enabled" = true;
-            # Delete this after https://gitlab.freedesktop.org/mesa/mesa/-/issues/8044
-            "media.mediasource.vp9.enabled" = false;
-            "media.ffvpx.enabled" = false;
             "dom.webgpu.enabled" = true;
+            
+
+            # VP9 fails to work with vaapi in framework so we have to disable it until this gets fixed
+            # # Delete this after https://gitlab.freedesktop.org/mesa/mesa/-/issues/8044
+            # This was a workaround but makes it vp9 only videos not to show in youtube
+            # "media.mediasource.vp9.enabled" = false;
+
+            # "media.ffmpeg.vaapi.enabled" = true;
+            # "media.ffvpx.enabled" = false;
+
           };
         };
       };
