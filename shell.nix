598798feba8be4c958ec701e0d6ae8fcec571630
leiserfg <leiserfg@gmail.com>
2024-09-01 11:39:26 +0200

    fix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,20 @@
+# Shell for bootstrapping flake-enabled nix and home-manager
 {
-  pkgs ?
-    import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz";
-    }) {},
-}: let
-  projectRoot = "${builtins.toString ./.}/src/inventorum.api";
-in
-  pkgs.mkShell {
-    buildInputs = [
-
-      pkgs.python312
-      pkgs.python312Packages.pip
-    ];
-    shellHook = ''
-    '';
-  }
+  pkgs ? let
+    # If pkgs is not defined, instantiate nixpkgs from locked commit
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+    system = builtins.currentSystem;
+    overlays = []; # Explicit blank overlay to avoid interference
+  in
+    import nixpkgs {inherit system overlays;},
+  ...
+}:
+pkgs.mkShell {
+  # Enable experimental features without having to specify the argument
+  NIX_CONFIG = "experimental-features = nix-command flakes";
+  nativeBuildInputs = with pkgs; [nix home-manager git];
+}
