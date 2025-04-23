b2d70b36b25f78e1621d76fea0b5bdcba6a48df1
leiserfg <leiserfg@gmail.com>
2025-04-23 18:11:02 +0200

    Wed Apr 23 06:11:02 PM CEST 2025

--- home/leiserfg/features/kitty.nix
+++ home/leiserfg/features/kitty.nix
@@ -22,9 +22,10 @@
       tab_separator = "│";
       enabled_layouts = "tall";
       enable_audio_bell = false;
+      text_composition_strategy = "1.3 0";
       # cursor_trail = 3;
       # Have to force it because some symbols are loaded from djavu otherwise
-      symbol_map = "U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0C8,U+E0CA,U+E0CC-U+E0D2,U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E634,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F500-U+F8FF,U+FD0B Symbols Nerd Font Mono";
+      symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono";
 
       editor = "nvim";
       scrollback_pager = ''nvim -c 'setlocal nonumber nolist showtabline=0 foldcolumn=0|Man!' -c "autocmd VimEnter * normal G" -'';
