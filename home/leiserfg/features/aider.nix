d3ac3a1db12c64fe298b8dd4e790bb4c6864f901
leiserfg <leiserfg@gmail.com>
2025-08-04 12:06:53 +0200

    Aider

--- home/leiserfg/features/aider.nix
+++ home/leiserfg/features/aider.nix
@@ -1,42 +1,33 @@
-{
-  pkgs,
-  home,
-  lib,
-  ...
-}:
-{
-
-  home.sessionVariables.AIDER_MODEL = "github_copilot/claude-sonnet-4";
-  home.packages = [
-    pkgs.aider-chat
+{ pkgs, ... }:
+let
+  yaml = pkgs.formats.yaml_1_2 { };
+  models = [
+    "gpt-4.1"
+    "claude-sonnet-4"
   ];
+  extra_params = {
+    max_tokens = 80000;
+    extra_headers = {
+      User-Agent = "GithubCopilot/1.155.0";
+      Editor-Plugin-Version = "copilot/1.155.0";
+      Editor-Version = "vscode/1.85.1";
+      Copilot-Integration-Id = "vscode-chat";
+    };
+  };
+in
+{
+  home.packages = [ pkgs.aider-chat ];
 
-  home.file.".aider.model.settings.yml".source =
-    (pkgs.formats.yaml_1_2 { }).generate "aider-models.yaml"
-      (
-        let
-          extra_params = {
-            max_tokens = 80000;
-            extra_headers = {
-              User-Agent = "GithubCopilot/1.155.0";
-              Editor-Plugin-Version = "copilot/1.155.0";
-              Editor-Version = "vscode/1.85.1";
-              Copilot-Integration-Id = "copilot-chat";
-            };
-          };
-        in
-        (builtins.map
-          (name: {
-            name = "github_copilot/${name}";
-            extra_params = extra_params;
-          })
-          [
-            "o4-mini"
-            "gemini-2.5-pro"
-            "claude-sonnet-4"
-            "gpt-4.1"
-          ]
-        )
-      );
+  home.file.".aider.conf.yml".source = yaml.generate "aider-conf" {
+    model = "github_copilot/claude-sonnet-4";
+    show-model-warnings = false;
+    auto-commits = false;
+  };
 
+  home.file.".aider.model.settings.yml".source = yaml.generate "aider-model-settings" (
+    builtins.map (model: {
+      name = "github_copilot/${model}";
+      inherit extra_params;
+    }) models
+  );
 }
