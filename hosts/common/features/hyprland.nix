634c75301463cb9cf28a2134abb7cbffc84804d6
leiserfg <leiserfg@gmail.com>
2025-08-21 09:56:25 +0200

    Update

--- hosts/common/features/hyprland.nix
+++ hosts/common/features/hyprland.nix
@@ -5,20 +5,34 @@
   outputs,
   unstablePkgs,
   ...
-}: {
+}:
+{
 
-  security.pam.services.hyprlock = {};
-  security.pam.services.swaylock = {};
+  security.pam.services.hyprlock = { };
+  security.pam.services.swaylock = { };
   environment.systemPackages = with pkgs; [
     swaylock
   ];
   services.greetd = {
     enable = true;
     settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --user-menu --remember";
-        user = "greeter";
-      };
+      default_session =
+        let
+          tuigreet = "${lib.getExe pkgs.tuigreet}";
+          tuigreetOptions = [
+            "--remember"
+            "--time"
+            "--user-menu"
+            # Make sure theme is wrapped in single quotes. See https://github.com/apognu/tuigreet/issues/147
+            "--theme 'border=blue;text=cyan;prompt=green;time=red;action=blue;button=white;container=black;input=red'"
+            "--cmd Hyprland"
+          ];
+          flags = lib.concatStringsSep " " tuigreetOptions;
+        in
+        {
+          command = "${tuigreet} ${flags}";
+          user = "greeter";
+        };
     };
   };
 }
