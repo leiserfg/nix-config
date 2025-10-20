856b2cf90d90cf0fc4b7a171102b988348bfba76
leiserfg <leiserfg@gmail.com>
2025-10-20 09:42:12 +0200

    update

--- hosts/common/features/laptop.nix
+++ hosts/common/features/laptop.nix
@@ -6,34 +6,6 @@
   ...
 }:
 {
-  # services.tlp = {
-  #   enable = true;
-  #   settings = {
-  #     RADEON_DPM_PERF_LEVEL_ON_AC = "high";
-  #     RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
-  #
-  #     RADEON_DPM_STATE_ON_AC = "performance";
-  #     RADEON_DPM_STATE_ON_BAT = "battery";
-  #
-  #     RADEON_POWER_PROFILE_ON_AC = "high";
-  #     RADEON_POWER_PROFILE_ON_BAT = "low";
-  #
-  #     PCIE_ASPM_ON_AC = "performance";
-  #     PCIE_ASPM_ON_BAT = "powersupersave";
-  #   };
-  # };
-  # services.auto-cpufreq.enable = true;
-  # services.auto-cpufreq.settings = {
-  #   battery = {
-  #     governor = "powersave";
-  #     turbo = "never";
-  #   };
-  #   charger = {
-  #     governor = "performance";
-  #     turbo = "auto";
-  #   };
-  # };
-
   powerManagement = {
     powerDownCommands = "${pkgs.util-linux}/bin/rfkill block all";
     powerUpCommands = "${pkgs.util-linux}/bin/rfkill unblock all";
