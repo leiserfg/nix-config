fc932ca50cb361321d101dda04949aa9fb6b9a25
leiserfg <leiserfg@gmail.com>
2026-07-13 10:20:19 +0200

    Add glslviewer override to fix audio nullptr bug

--- overlay/default.nix
+++ overlay/default.nix
@@ -7,9 +7,14 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+    # Fix glslviewer audio bug: Remove the incorrect m_dft_buffer = nullptr line
+    # that was added in commit 79078eb ("Fix in-class initialization")
+    # Bug: https://github.com/patriciogonzalezvivo/glslViewer/issues/...
+    glslviewer = prev.glslviewer.overrideAttrs (oldAttrs: {
+      patches = (oldAttrs.patches or []) ++ [
+        ./patches/glslviewer-fix-audio-nullptr.patch
+      ];
+    });
   };
 in
   inputs.nixpkgs.lib.composeManyExtensions [additions modifications]
