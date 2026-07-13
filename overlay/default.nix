940dc7ed70389d7bf092d912717041d0fbceb9d1
leiserfg <leiserfg@gmail.com>
2026-07-13 10:21:42 +0200

    Fix inline patch scope in overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -3,20 +3,6 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs {pkgs = final;};
 
-  # Patch for glslviewer audio bug
-  glslviewer-audio-patch = final.writeText "glslviewer-fix-audio-nullptr.patch" ''
-    --- a/src/gl/textureStreamAudio.cpp
-    +++ b/src/gl/textureStreamAudio.cpp
-    @@ -58,7 +58,6 @@ TextureStreamAudio::TextureStreamAudio(): TextureStream() {
-         m_dft_buffer = (float*)av_malloc_array(sizeof(float), m_buf_len);
-         m_buffer_wr.resize(m_buf_len, 0);
-         m_buffer_re.resize(m_buf_len, 0);
-    -    m_dft_buffer = nullptr;
-     }
-     
-     TextureStreamAudio::~TextureStreamAudio() {
-  '';
-
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
@@ -26,7 +12,18 @@
     # Bug: https://github.com/patriciogonzalezvivo/glslViewer/issues/...
     glslviewer = prev.glslviewer.overrideAttrs (oldAttrs: {
       patches = (oldAttrs.patches or []) ++ [
-        (glslviewer-audio-patch final)
+        (final.writeText "glslviewer-fix-audio-nullptr.patch" ''
+          --- a/src/gl/textureStreamAudio.cpp
+          +++ b/src/gl/textureStreamAudio.cpp
+          @@ -58,7 +58,6 @@ TextureStreamAudio::TextureStreamAudio(): TextureStream() {
+               m_dft_buffer = (float*)av_malloc_array(sizeof(float), m_buf_len);
+               m_buffer_wr.resize(m_buf_len, 0);
+               m_buffer_re.resize(m_buf_len, 0);
+          -    m_dft_buffer = nullptr;
+           }
+           
+           TextureStreamAudio::~TextureStreamAudio() {
+        '')
       ];
     });
   };
