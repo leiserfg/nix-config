23def147a7d9d421054d26761f99d41983ff695d
leiserfg <leiserfg@gmail.com>
2026-08-26 23:30:53 +0200

    Wed Aug 26 11:31:00 PM CEST 2026

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,9 +1,8 @@
-{lib, ...}: {
+{ lib, ... }: {
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
-    supportedLocales = lib.mkDefault [
-      "en_US.UTF-8/UTF-8"
-    ];
+    extraLocaleSettings.LC_TIME = "en_GB.UTF-8"; # European datetime
   };
   time.timeZone = lib.mkDefault "Europe/Berlin";
+
 }
